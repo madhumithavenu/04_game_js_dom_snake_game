0879f33b775ae715a6695819f15ac2197511cd60
Madhumitha <madhumithavenu03@gmail.com>
2023-09-15 21:08:54 +0530

    food visibility

---
+++ js/index.js
@@ -0,0 +1,38 @@
+//Game Constants  & variables
+let direction = { x: 0, y: 0 };
+const foodSound = new Audio('music/food.mp3');
+const gameOverSound = new Audio('music/gameover.mp3');
+const moveSound = new Audio('music/move.mp3');
+const musicSound = new Audio('music/music.mp3');
+let speed = 2;
+let lastPaintTime =0;
+let snakeArr = [
+    {x: 13, y: 15}
+]
+
+//Game Functions
+function main(ctime){
+    window.requestAnimationFrame(main);
+    // console.log(ctime)
+    if((ctime - lastPaintTime)/1000 < 1/speed){
+        return;
+    }
+    lastPaintTime = ctime;
+    gameEngine();
+}
+function gameEngine(){
+    //Part 1: Updating the snake array
+
+    //Part 2 : Render the snake and Food
+    board.innerHTML = "";
+    snakeArr.forEach((e,index)=>{
+        snakeElement = document.createElement('div');
+        snakeElement.style.gridRowStart = e.y;
+        snakeElement.style.gridColumnStart = e.x;
+        snakeElement.classList.add('food');
+        board.appendChild(snakeElement);
+    })
+}
+
+// Main logic starts here
+window.requestAnimationFrame(main);
