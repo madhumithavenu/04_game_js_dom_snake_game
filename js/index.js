db1986e7f5c54f2c3225690d3fbae4f3925413e9
Madhumitha <madhumithavenu03@gmail.com>
2023-09-15 21:53:29 +0530

    Collision of snake

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const foodSound = new Audio('music/food.mp3');
 const gameOverSound = new Audio('music/gameover.mp3');
 const moveSound = new Audio('music/move.mp3');
 const musicSound = new Audio('music/music.mp3');
-let speed = 2;
+let speed = 4;
 let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [
@@ -22,8 +22,20 @@ function main(ctime) {
     lastPaintTime = ctime;
     gameEngine();
 }
-function isCollide(sarr){
-    return false;
+function isCollide(snake){
+    // If you bump into yourself 
+    for (let i = 1; i < snakeArr.length; i++) {
+        if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){
+            return true;
+        }
+    }
+    // If you bump into the wall
+    if(snake[0].x >= 18 || snake[0].x <=0 || snake[0].y >= 18 || snake[0].y <=0){
+        return true;
+    }
+        
+   
+
 }
 function gameEngine() {
     //Part 1: Updating the snake array
@@ -39,12 +51,7 @@ function gameEngine() {
     // if food is eaten increment the score and regenerate the food
     if(snakeArr[0].y === food.y && snakeArr[0].x ===food.x){
         foodSound.play();
-        score += 1;
-        if(score>hiscoreval){
-            hiscoreval = score;
-            localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
-            hiscoreBox.innerHTML = "HiScore: " + hiscoreval;
-        }
+        
         scoreBox.innerHTML = "Score: " + score;
         snakeArr.unshift({x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y});
         let a = 2;
